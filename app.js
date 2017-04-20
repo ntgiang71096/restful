af34d96665cd579eded6eb624b7209ae732c3425
Giangnt <nguyentruonggiang71096@gmail.com>
2017-04-20 22:30:37 +0700

    add db

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var mongo = require('mongodb');
 var monk = require('monk');
-var db = monk('mongodb://nguyentruonggiang:giang123@ds111851.mlab.com:11851/cdcn');
+var db = monk('mongodb://enamoria:Kinh8396@ds161190.mlab.com:61190/node');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
